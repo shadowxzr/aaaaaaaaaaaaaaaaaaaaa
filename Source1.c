32b056f54cdbd81edec4cb64f32db872eff1a9af
user15 <roltgy.56@gmail.com>
2025-12-18 14:10:10 +0300

    com12

--- Source1.c
+++ Source1.c
@@ -2,7 +2,10 @@
 #include <Windows.h>
 
 void main{
-	printf("Hello world -25!\n")
-	printf("fffffffffffffffff \n")
-	printf("Goodbye world156!\n")
+	printf("Hello world -25!\n");
+	printf("fffffffffffffffff \n");
+	printf("Goodbye world156!\n");
+		printf("Goodbye world15       6!\n");
+
+
 }
